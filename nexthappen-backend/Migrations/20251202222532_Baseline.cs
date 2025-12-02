79b04bec1e8fbead75a97657ddee8ae4b45348ae
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-12-02 17:29:23 -0500

    Fix migrations baseline + photos json support

--- nexthappen-backend/Migrations/20251202222532_Baseline.cs
+++ nexthappen-backend/Migrations/20251202222532_Baseline.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace nexthappen_backend.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class Baseline : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            
+
         }
     }
 }
